61df9e229f0f616b8969f9602890de9ff86ea708
Wizzerrd <luis@DESKTOP-7AS3T51>
2023-07-21 15:28:37 -0700

    Add Buttons, Mouse input

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import * as Matter from 'matter-js'; // gotta import the files !!!
+import * as MatterWrap from 'matter-wrap';
+Matter.use(MatterWrap);
 
 document.addEventListener("DOMContentLoaded", ()=>{console.log('hello world')}) 
 //logs to console when the document object (provided by DOM API) is loaded
@@ -8,13 +10,20 @@ var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+    Composite = Matter.Composite,
+    Body = Matter.Body,
+    Mouse = Matter.Mouse,
+    Events = Matter.Events;
+
+// Matter.Bodies.prototype.onClick = function(){
+//     console.log("banana")
+// };
 
 // create an engine
 var engine = Engine.create({gravity: {
     scale: 0.001,
     x: 0,
-    y: 0
+    y: 1
 }});
 // engine default gravity params:
 // scale: 0.001
@@ -26,26 +35,34 @@ var engine = Engine.create({gravity: {
 // element: HTMLElement is converted into a canvas upon which the simulation is rendered
 var render = Render.create({
     element: document.querySelector('#simulator'),
-    engine: engine,
-    background: '#ff0000' 
+    engine: engine
 });
 
 // create a box and a circle
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var ball = Bodies.circle(450, 50, 80);
+var ball = Bodies.circle(450, 50, 80, {
+    plugin: {
+        wrap: {
+            min: {
+                x: 0
+            },
+            max: {
+                x: 800
+            }
+        }
+    }
+});
 
 // create boundaries
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-var rightBound = Bodies.rectangle(-10, 300, 60, 610, { isStatic: true });
-var leftBound = Bodies.rectangle(810, 300, 60, 610, { isStatic: true });
-var roof = Bodies.rectangle(400, -10, 810, 60, { isStatic: true });
-Bodies.rectangle()
+var ground = Bodies.rectangle(400, 610, 2000, 60, { isStatic: true });
+// var rightBound = Bodies.rectangle(-10, 300, 60, 610, { isStatic: true });
+// var leftBound = Bodies.rectangle(810, 300, 60, 610, { isStatic: true });
+// var roof = Bodies.rectangle(400, -10, 810, 60, { isStatic: true });
 
 // add boundaries to the world
-Composite.add(engine.world, [ground,rightBound,leftBound,roof])
+Composite.add(engine.world, [ground])
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, ball]);
+Composite.add(engine.world, [ball]);
 
 // run the renderer
 Render.run(render);
@@ -55,3 +72,46 @@ var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+// Buttons
+
+var hButton = document.querySelector('#horizontal-force');
+var vButton = document.querySelector('#vertical-force');
+var pushButton = document.querySelector('#push-pull');
+
+hButton.addEventListener("click", ()=> {
+    Body.applyForce(ball, ball.position, {x: 1, y:0});
+})
+
+vButton.addEventListener("click", ()=> {
+    Body.applyForce(ball, ball.position, {x:0, y:-1});
+})
+
+// Mouse Input
+
+var simulator = document.querySelector('#simulator');
+var simRect = simulator.getBoundingClientRect();
+
+let pushBool = true;
+
+pushButton.addEventListener("click", ()=> {
+    if(pushBool){
+        pushBool = false;
+        pushButton.innerHTML = "Pull";
+    } else {
+        pushBool = true;
+        pushButton.innerHTML = "Push";
+    }
+})
+
+simulator.addEventListener("click", (e)=> {
+    let simPos = {x: e.clientX - simRect.x , y: e.clientY - simRect.y};
+    let clickMult = -0.002;
+    if(pushBool) {
+        clickMult = -0.002
+    } else {
+        clickMult = 0.002
+    }
+    let clickVect = {x: clickMult * (simPos.x - ball.position.x), y: clickMult * (simPos.y - ball.position.y)};
+    Body.applyForce(ball, simPos, clickVect);
+})
